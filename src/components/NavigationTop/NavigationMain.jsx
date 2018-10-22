401364b2c6c9e1548c18bd4412d7dfb0f42a4693
Anthony Tran <anthonytran.dev@gmail.com>
2018-10-22 14:01:59 +0100

    Feat NavigationMain init

---
+++ src/components/NavigationTop/NavigationMain.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import NavigationSection from "./NavigationSection";
+import LogoCompany from "./logo-header.svg";
+import IconHamburger from "./icon-hamburger.svg";
+
+const NavigationMain = () => (
+  <div className="nav-main">
+    <img
+      className="nav-main__img"
+      src={LogoCompany}
+      alt="sputnik company logo"
+    />
+    <NavigationSection />
+    <img
+      className="nav-main__icon nav-main__icon--hamburger"
+      src={IconHamburger}
+      alt="hamburger menu"
+    />
+  </div>
+);
+
+export default NavigationMain;
