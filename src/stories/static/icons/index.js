c404a403b757662acd6161a9735c8073c394ef88
Anthony Tran <anthonytran.dev@gmail.com>
2018-10-22 15:15:39 +0100

    Test stories/../icons init

---
+++ src/stories/static/icons/index.js
@@ -0,0 +1,6 @@
+import IconArrow from "./Icon-Arrow.svg";
+import IconMinus from "./Icon-Minus.svg";
+import IconPlus from "./Icon-Plus.svg";
+import IconSearch from "./Icon-Search.svg";
+
+export { IconArrow, IconMinus, IconPlus, IconSearch };
